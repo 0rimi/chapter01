8ce63d05b9f85a96e7ea97c4344134c9e2a3be20
이영림 <rm1113@naver.com>
2021-11-17 13:51:27 +0900

    문제풀면서 commit

--- src/com/javaex/ex04/Ex04.java
+++ src/com/javaex/ex04/Ex04.java
@@ -8,10 +8,11 @@ public class Ex04 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-			int num = 0;
+			int sum = 0;
+			
 			for(int i=1; i<=10; i++) {
-			 num = num+i;
-			 System.out.println(i+" 까지의 합은 "+num);
+			 sum = sum+i;
+			 System.out.println(i+" 까지의 합은 "+sum);
 			}
 			
 		sc.close();
