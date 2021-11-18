e59c570262064f4b4bd960d9ba1973aaf4ade961
이영림 <rm1113@naver.com>
2021-11-18 16:35:26 +0900

    do while

---
+++ src/com/javaex/ex04/Ex12.java
@@ -0,0 +1,48 @@
+package com.javaex.ex04;
+
+import java.util.Scanner;
+
+public class Ex12 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		//do while문
+		
+		System.out.println("숫자를 입력하세요.[0이면 종료]");
+		
+		int sum = 0;
+		int num;
+		
+		do {
+			num ㄴ= sc. nextInt();
+			sum = sum+num;
+			System.out.println("합계 : "+sum);
+		}
+		while(num!=0);
+		
+		System.out.println("종료");
+		
+		
+		/*
+		System.out.println("숫자를 입력하세요.[0이면 종료]");
+		
+		int sum=0;
+		
+		while(true){
+			int num = sc.nextInt();
+			if(num==0) {
+				System.out.println("종료");
+				break;
+			}
+			else if(num!=0) {
+				sum=sum+num;
+				System.out.println("합계 : "+sum);	
+			}
+			}
+		*/
+		sc.close();
+		
+	}
+
+}
