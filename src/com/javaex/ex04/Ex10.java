42c8eb136696249ed3cddbffe21481f6b07be3eb
이영림 <rm1113@naver.com>
2021-11-18 14:28:51 +0900

    11/18 수업분

--- src/com/javaex/ex04/Ex10.java
+++ src/com/javaex/ex04/Ex10.java
@@ -3,15 +3,21 @@ package com.javaex.ex04;
 public class Ex10 {
 
 	public static void main(String[] args) {
-		//아직...강사님이...안해주셨습니다아...
-		for (int i=1; i<=20; i++) {
-			if(i%2!=0||i%3!=0) {
-				System.out.println(i);
+		
+		for(int i=1; i<=10; i++) {
+			if(i%2==0&&i%3==0) {
+				break;
+			}
+			System.out.println(i);
+		}
+		
+		System.out.println("===================");
+		
+		for(int i=1; i<=10; i++) {
+			if(i%2==0&&i%3==0) {
 				continue;
 			}
-			else if(i%2==0&&i%3==0) {
-				continue;
-			}
+			System.out.println(i);
 		}
 
 	}
